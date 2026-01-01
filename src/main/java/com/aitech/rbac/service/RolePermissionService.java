83c8e276411f1ba758433604ec10eb84bbc37923
ethan <thanhdoquang@live.com>
2026-01-01 21:52:45 +0700

    update code add front example

--- src/main/java/com/aitech/rbac/service/RolePermissionService.java
+++
@@ -1,8 +0,0 @@
-package com.aitech.rbac.service;
-
-import com.aitech.rbac.model.RolePermission;
-
-public interface RolePermissionService {
-    void create(RolePermission entity);
-    void delete(RolePermission entity);
-}
